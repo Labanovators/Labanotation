d06619dcb4d00814e9d9c240c5c02db1cbf5a924
alexhatch <alexhatchh@gmail.com>
2023-11-08 17:15:47 -0800

    fixed venv for running script

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ mkdir uploads
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
-pip install flask
+pip install Flask
 pip install opencv-python
 pip install numpy
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ./install.sh
+source venv/bin/activate
 python3 web_browser.py
